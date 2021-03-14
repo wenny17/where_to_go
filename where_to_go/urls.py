bafa665398e69b87c7b9e91aadc19be2b15f30b8
MaksBakmaev <temnepatypa17@gmail.com>
2021-03-14 14:01:41 +0200

    Add blank start page

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import start_page
+
 urlpatterns = [
+    path('', start_page),
     path('admin/', admin.site.urls),
 ]
---
+++ where_to_go/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+
+def start_page(request):
+    return HttpResponse("""
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>Стартовая</title>
+</head>
+<body>
+  <h1>Здесь будет карта</h1>
+</body>
+</html>
+    """)
