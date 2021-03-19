bbf42261ca256040da4bcdccdd4d1287fba0794a
MaksBakmaev <temnepatypa17@gmail.com>
2021-03-19 18:02:58 +0200

    Add static root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -130,10 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
+
+# Media Folder Settings
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
