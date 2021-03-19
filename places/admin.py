a69c272b319d66e6e4437bf0452b959567a895bf
MaksBakmaev <temnepatypa17@gmail.com>
2021-03-19 16:42:14 +0200

    Add sortable images preview

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from adminsortable2.admin import SortableInlineAdminMixin
 
 from .models import Place, PlaceImage
+# Register your models here.
 
 
-admin.site.register(Place)
-admin.site.register(PlaceImage)
+class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = PlaceImage
+    readonly_fields = ['preview']
+    extra = 0
+
+    def preview(self, image):
+        return format_html('<img src="{}" height="200"></img>', image.image.url)
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
